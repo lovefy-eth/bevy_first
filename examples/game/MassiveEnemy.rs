664f194eb41cd3dc0d2dfc838033521b952db6a3
yibana <lovefy.eth@gmail.com>
2025-04-03 15:05:04 +0800

    武器+子弹动画

--- examples/game/MassiveEnemy.rs
+++ examples/game/MassiveEnemy.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::PI;
+use bevy::input::mouse::MouseButtonInput;
 use bevy::math::vec3;
 use bevy::prelude::*;
 // Window
@@ -11,7 +13,11 @@ const WH: f32 = 900.0;
 
 // Sprites
 const SPRITE_SCALE_FACTOR: f32 = 3.0;
-const SPRITE_SPEED:f32 = 2.0;
+const SPRITE_SPEED: f32 = 2.0;
+
+//GUN
+const BULLET_SPEED: f32 = 2.0;
+const BULLET_SPAWN_INTERVAL: f32 = 0.1;
 
 // Colors
 const BG_COLOR: (u8, u8, u8) = (192, 204, 184);
@@ -29,10 +35,23 @@ enum GameState {
 struct GlobalTextureAtlasHandle(Option<Handle<TextureAtlasLayout>>);
 #[derive(Resource)]
 struct GlobalSpriteSheetHandle(Option<Handle<Image>>);
+#[derive(Resource)]
+struct CurPosition(Option<Vec2>);
+#[derive(Component)]
+struct GunTimer(Timer);
+
 
 // Components
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct Gun;
+#[derive(Component)]
+struct Bullet;
+#[derive(Component)]
+struct BulletDirection(Vec3);
+
 
 fn main() {
     App::new()
@@ -56,6 +75,8 @@ fn main() {
         // custom resources
         .insert_resource(GlobalTextureAtlasHandle(None))
         .insert_resource(GlobalSpriteSheetHandle(None))
+        .insert_resource(CurPosition(None))
+        //.insert_resource(GunTimer(Timer::from_seconds(0.1,TimerMode::Repeating)))
         // .add_plugins(LogDiagnosticsPlugin::default())
         // .add_plugins(FrameTimeDiagnosticsPlugin)
         // systems
@@ -65,7 +86,14 @@ fn main() {
             Update,
             (
                 close_on_esc,
-                player_input_system.run_if(in_state(GameState::InGame)),
+                (
+                    player_input_system,
+                    update_gun_transform,
+                    update_gun_input,
+                    update_cursor_position,
+                    update_bullet,
+                )
+                    .run_if(in_state(GameState::InGame)),
             ),
         )
         .run();
@@ -110,6 +138,20 @@ fn init_world(
         Transform::from_scale(Vec3::splat(SPRITE_SCALE_FACTOR)),
         Player,
     ));
+    commands.spawn((
+        Sprite {
+            image: image_handle.0.clone().unwrap(),
+            texture_atlas: Some(TextureAtlas {
+                layout: texture_atlas.0.clone().unwrap(),
+                index: 8,
+            }),
+            ..default()
+        },
+        Transform::from_scale(Vec3::splat(SPRITE_SCALE_FACTOR)),
+        Gun,
+        GunTimer(Timer::from_seconds(BULLET_SPAWN_INTERVAL,TimerMode::Repeating)),
+    ));
+
     state.set(GameState::InGame);
 }
 
@@ -134,17 +176,18 @@ fn close_on_esc(
 }
 
 fn player_input_system(
-    mut player_query: Query<&mut Transform,With<Player>>,
-    keyboard_input: Res<ButtonInput<KeyCode>>
+    mut player_query: Query<&mut Transform, With<Player>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
 ) {
-    if player_query.is_empty(){
+    if player_query.is_empty() {
         return;
     }
     let mut delta = Vec2::ZERO;
     let w_key = keyboard_input.pressed(KeyCode::KeyW) || keyboard_input.pressed(KeyCode::ArrowUp);
     let a_key = keyboard_input.pressed(KeyCode::KeyA) || keyboard_input.pressed(KeyCode::ArrowLeft);
     let s_key = keyboard_input.pressed(KeyCode::KeyS) || keyboard_input.pressed(KeyCode::ArrowDown);
-    let d_key = keyboard_input.pressed(KeyCode::KeyD) || keyboard_input.pressed(KeyCode::ArrowRight);
+    let d_key =
+        keyboard_input.pressed(KeyCode::KeyD) || keyboard_input.pressed(KeyCode::ArrowRight);
     if w_key || a_key || s_key || d_key || d_key {
         if w_key {
             delta.y += 1.0;
@@ -159,8 +202,99 @@ fn player_input_system(
             delta.x += 1.0;
         }
         delta = delta.normalize_or_zero();
-
-        let mut transform = player_query.single_mut();
-        transform.translation+=vec3(delta.x, delta.y, 0.0) * SPRITE_SPEED;
+        if delta.is_finite() {
+            let mut transform = player_query.single_mut();
+            transform.translation += vec3(delta.x, delta.y, 0.0) * SPRITE_SPEED;
+        }
     }
+}
+
+fn update_bullet(
+    mut bullet_query: Query<(&mut Transform, &mut Bullet,&BulletDirection)>,
+){
+    if bullet_query.is_empty(){
+        return;
+    }
+    for (mut transform, mut bulled, direction) in bullet_query.iter_mut(){
+        transform.translation+=direction.0.normalize() * SPRITE_SPEED;
+    }
+}
+fn update_gun_input(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut gun_query: Query<(&Transform,&mut GunTimer), With<Gun>>,
+    texture_atlas: Res<GlobalTextureAtlasHandle>,
+    image_handle: Res<GlobalSpriteSheetHandle>,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+){
+    if !mouse_button_input.pressed(MouseButton::Left) {
+        return;
+    }
+    let (gun_transform, mut gun_time) = gun_query.single_mut();
+    if  gun_time.0.tick(time.delta()).finished() {
+        let gun_pos = gun_transform.translation;
+        commands.spawn((
+            Sprite {
+                image: image_handle.0.clone().unwrap(),
+                texture_atlas: Some(TextureAtlas {
+                    layout: texture_atlas.0.clone().unwrap(),
+                    index: 9,
+                }),
+                ..default()
+            },
+            Transform::from_translation(gun_pos),
+            Bullet,
+            BulletDirection(*gun_transform.local_y()),
+        ));
+    }
+
+}
+
+fn update_gun_transform(
+    cursor_pos: Res<CurPosition>,
+    player_query: Query<&Transform, With<Player>>,
+    mut gun_query: Query<&mut Transform, (With<Gun>, Without<Player>)>,
+) {
+    if player_query.is_empty() || gun_query.is_empty() {
+        return;
+    }
+    let player_pos = player_query.single().translation;
+    let mut gun_transform = gun_query.single_mut();
+    let cursor_pos = match cursor_pos.0 {
+        Some(pos) => pos,
+        None => player_pos.truncate(),
+    };
+
+    let to_player = (cursor_pos.xy() - player_pos.xy()).normalize();
+    let angle = to_player.y.atan2(to_player.x) ;
+    let rotate_to_player = Quat::from_rotation_arc(Vec3::Y, to_player.extend(0.0));
+    gun_transform.rotation = rotate_to_player;//Quat::from_rotation_z(angle);
+    let offset = 60.0;
+    gun_transform.translation = player_pos + vec3(
+        offset*angle.cos(),
+        offset*angle.sin(),
+        0.0);
+}
+
+fn update_cursor_position(
+    camera_query: Single<(&Camera, &GlobalTransform)>,
+    windows: Query<&Window>,
+    mut cursor_pos: ResMut<CurPosition>,
+) {
+    let (camera, camera_transform) = *camera_query;
+
+    let Ok(window) = windows.get_single() else {
+        return;
+    };
+
+    let Some(cursor_position) = window.cursor_position() else {
+        return;
+    };
+
+    // Calculate a world position based on the cursor's position.
+    let Ok(point) = camera.viewport_to_world_2d(camera_transform, cursor_position) else {
+        return;
+    };
+
+    cursor_pos.0 = Some(point);
 }
